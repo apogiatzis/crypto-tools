c7d7184f19133790c7411ccfed50b1672a8d66e8
Antreas Poyiatzis <apogiatzis@lookingglasscyber.com>
2018-07-15 15:07:43 -0400

    added pub2int

---
+++ RSA/pub2int.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "[-] Public Key File: $1"
+openssl rsa -pubin -inform PEM -text -noout < $1  > pub2int.dat
+
+echo '[+] Modulus'
+modulus=$(awk '/Modulus:/{flag=1;next}/Exponent/{flag=0}flag' < pub2int.dat | tr -d ":    \n")
+echo $modulus
+echo -e "\n"
+
+echo "[+] Exponent"
+exponent=$(awk '/Exponent:/,0' < pub2int.dat | tail -n +2 | tr -d ":    \n")
+echo $exponent
+echo -e "\n"
+
+rm pub2int.dat
